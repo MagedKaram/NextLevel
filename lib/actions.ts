5a8a257d1333f76ab86765b94360527f3262505d
MagedKaram <maged31009445@f-eng.tanta.edu.eg>
2026-02-11 18:32:56 +0200

    adding server side validation

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,11 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { createMeal } from "./meals";
+
+function validateMealData(text: string) {
+  return text! || text.trim() === " ";
+}
 
 export async function shareMeal(formData: FormData) {
   const mealData = {
@@ -11,5 +16,17 @@ export async function shareMeal(formData: FormData) {
     creator_email: formData.get("email")?.toString() || "",
     image: formData.get("image"),
   };
+  if (
+    !validateMealData(mealData.title) ||
+    !validateMealData(mealData.summary) ||
+    !validateMealData(mealData.instructions) ||
+    !validateMealData(mealData.creator) ||
+    !validateMealData(mealData.creator_email) ||
+    !mealData.image ||
+    (mealData.image instanceof File && mealData.image.size === 0)
+  ) {
+    throw new Error("Invalid meal data");
+  }
   await createMeal(mealData);
+  redirect("/meals");
 }
